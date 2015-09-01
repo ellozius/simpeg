8ea2eba5db5055ac8d5a1c8f7f9a8bb1afdb615d
Edy Zulyza <edy.zulyza@gmail.com>
2015-09-01 14:12:42 +0700

    update 2015-09-01 14:12

--- src/main/java/id/go/riau/bkd/simpeg/entity/User.java
+++ src/main/java/id/go/riau/bkd/simpeg/entity/User.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package id.go.riau.bkd.simpeg.entity;
 
 import javax.persistence.Column;
